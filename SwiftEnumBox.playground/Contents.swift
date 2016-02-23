e59bca82e029d14feffd7875ac88561e27d9ecd2
Alexey Patosin <azyktulek@gmail.com>
2016-02-23 15:16:12 +0000

    initial commit

---
+++ SwiftEnumBox.playground/Contents.swift
@@ -0,0 +1,42 @@
+enum FieldId: String {
+    case Name = "Name_Field"
+    case Phone = "Phone_Field"
+    case EMail = "Email_Field"
+    
+    // Enum with raw type cannot have cases with arguments
+    //    case Custom(String)
+}
+
+enum FieldIdBox {
+    case SpecifiedFieldId(FieldId)
+    case Custom(String)
+    
+    init(fieldId: String) {
+        if let fieldId = FieldId(rawValue: fieldId) {
+            self = .SpecifiedFieldId(fieldId)
+        }
+        else {
+            self = .Custom(fieldId)
+        }
+    }
+    
+    func rawValue() -> String {
+        switch self {
+        case .SpecifiedFieldId(let fieldId):
+            return fieldId.rawValue
+        case .Custom(let fieldIdValue):
+            return fieldIdValue
+        }
+    }
+}
+
+////
+
+let fields = [
+    FieldIdBox(fieldId: "Name_Field"),
+    FieldIdBox(fieldId: "Phone_Field"),
+    FieldIdBox(fieldId: "Email_Field"),
+    FieldIdBox(fieldId: "Address_Field"),       // custom field
+]
+
+print(fields.map { $0.rawValue() })
